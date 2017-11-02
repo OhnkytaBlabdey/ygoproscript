97db224af7b46b0b565407c87c84ad57976862b6
OhnkytaBlabdey <1263189143@qq.com>
2017-11-02 16:31:57 +0800

    puzzle_creator.lua

---
+++ puzzle_creator.lua
@@ -0,0 +1,7 @@
+Debug.SetAIName("残局")
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI)
+Debug.SetPlayerInfo(0,100,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+Debug.AddCard(111021537,0,0,LOCATION_HAND,3,POS_FACEUP_ATTACK) --setcard
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
